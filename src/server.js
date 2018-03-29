9453a65cc00dc13d8da9ec2bbe0e2d4964c10eff
Herve <h.hochart29@gmail.com>
2018-03-29 18:33:27 +0200

    changing port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const server = express()
 const puppeteer = require('puppeteer')
 
-const PORT_LISTENING = process.env.PORT
+const PORT_LISTENING = process.env.PORT || 8080
 
 // Allow CORS
 server.use(function (req, res, next) {
